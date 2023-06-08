ca3214933f02bcb267b758efe39ec037c54777ff
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2023-06-08 17:26:21 -0500

    added colors and nav space adjustment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,51 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grayBlue: {
+          light: "#7A8696",
+          core: "#5B6470",
+          dark: "#3C424A",
+        },
+        darkBlue: {
+          light: "#140464",
+          core: "#16044B",
+          dark: "#0B0225",
+        },
+        darkPurple: {
+          light: "#8E78BA",
+          core: "#54476E",
+          dark: "#372F48",
+        },
+        warningRed: "#FF2850",
+        alertOrange: {
+          light: "#FF9E37",
+          core: "#FF9037",
+          dark: "#F67F36",
+        },
+        goGreen: {
+          light: "#34FFCA",
+          core: "#34FFBC",
+          dark: "#2EE09A",
+        },
+        skyBlue: {
+          light: "#a7ebff",
+          core: "#95d7e3",
+          dark: "#73a6b0",
+        },
+        lila: {
+          light: "#c2b8ff",
+          core: "#b8c0ff",
+          dark: "#9399cc",
+        },
+        tan: {
+          light: "#ffe4c5",
+          core: "#dabda9",
+          dark: "#a69081",
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
