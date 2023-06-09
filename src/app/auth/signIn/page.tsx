ad9deed25e6e31b8f9a4e2a89efdccfcbc493786
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2023-06-09 17:39:13 -0500

    more setup

--- src/app/auth/signIn/page.tsx
+++ src/app/auth/signIn/page.tsx
@@ -1,33 +1,60 @@
-"use client";
-import { getProviders, signIn } from "next-auth/react";
-import React, { useRef } from "react";
-
-
+'use client';
+import { getProviders, signIn } from 'next-auth/react';
+import React, { useRef, useState, useEffect } from 'react';
 
 const listProviders = async () => {
-    return {
-        providers: await getProviders(),
-    };
+	return {
+		providers: await getProviders(),
+	};
+};
+interface provider {
+	id: string;
+	name: string;
+	type: string;
+	signinUrl: string;
+	callbackUrl: string;
 }
+const Button = ({ provider }: { provider: provider }) => {
+	const { id, name, type, signinUrl, callbackUrl } = provider;
+	const onSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		console.log('is clicking');
+		const result = await signIn(id, {
+			callbackUrl: callbackUrl,
+			redirect: false,
+		});
+		console.log(result);
+		e.preventDefault();
+	};
+	return (
+		<button onClick={void onSubmit} className="rounded-lg bg-red-200 px-3 py-2">
+			{provider.name}
+		</button>
+	);
+};
 
-const LoginPage =  () => {
-    const userName = useRef("");
-    const pass = useRef(""); 
-    listProviders().then((data)=>{
-        console.log(data);
-    }).catch((e)=>{
-        console.log(e);
-    });
+const LoginPage = () => {
+	const [data, setData] = useState<provider[]>([]);
+	useEffect(() => {
+		listProviders()
+			.then(data => {
+				setData(Object.values(data.providers ? data.providers : {}));
+			})
+			.catch(e => {
+				console.log(e);
+			});
+	}, []);
 
-    return (
-        <div
-            className={
-                "flex flex-col justify-center items-center  h-screen bg-gradient-to-br gap-1 from-cyan-300 to-sky-600"
-            }
-        >
-            Sign in
-        </div>
-    );
+	return (
+		<div
+			className={
+				'flex h-screen flex-col items-center  justify-center gap-1 bg-gradient-to-br from-cyan-300 to-sky-600'
+			}
+		>
+			{data.map((provider: provider, i) => {
+				return <Button provider={provider} key={`${i}-${provider.id}`}></Button>;
+			})}
+		</div>
+	);
 };
 
 export default LoginPage;
